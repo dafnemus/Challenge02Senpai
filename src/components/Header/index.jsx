6ef77a4de9df815c4b7b4cf0066c926499db0c72
dafnemus <dafnemus@gmail.com>
2023-07-05 15:26:39 -0300

    define styles into Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,17 @@
 import moon from "../../assets/images/icon-moon.svg";
+import bgLight from "../../assets/images/bg-mobile-light.jpg";
+import styles from "./styles.module.css";
 
 const Header = () => {
   return (
-    <div className="">
-      <h1>TODO</h1>
-      <button>
-        <img src={moon} alt="moon" />
-      </button>
+    <div className={styles.container}>
+      <img src={bgLight} alt="mountains background" />
+      <div className={styles.mode}>
+        <h1>TODO</h1>
+        <button>
+          <img src={moon} alt="moon" />
+        </button>
+      </div>
     </div>
   );
 };
