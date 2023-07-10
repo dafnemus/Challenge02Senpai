6748688bdc08fdc9dfd8a6902f3c94c1b94094d0
dafnemus <dafnemus@gmail.com>
2023-07-10 14:50:12 -0300

    complete header and added light and dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import "./App.css";
+import "./styles.css";
 
 function App() {
+  const [light, setLight] = useState(true);
+  const handleTheme = () => {
+    setLight(!light);
+  };
   return (
-    <>
-      <Header />
-    </>
+    <div className={`container ${light ? "containerLight" : "containerDark"}`}>
+      <Header onClick={handleTheme} theme={light} />
+    </div>
   );
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,18 @@
+/* eslint-disable react/prop-types */
 import moon from "../../assets/images/icon-moon.svg";
+import sun from "../../assets/images/icon-sun.svg";
 import bgLight from "../../assets/images/bg-mobile-light.jpg";
+import bgDark from "../../assets/images/bg-mobile-dark.jpg";
 import styles from "./styles.module.css";
 
-const Header = () => {
+const Header = ({ onClick, theme }) => {
   return (
     <div className={styles.container}>
-      <img src={bgLight} alt="mountains background" />
+      <img src={theme ? bgLight : bgDark} alt="mountains background" />
       <div className={styles.mode}>
-        <h1>TODO</h1>
-        <button>
-          <img src={moon} alt="moon" />
+        <h1 className={styles.title}>TODO</h1>
+        <button onClick={onClick}>
+          <img src={theme ? moon : sun} alt="moon" />
         </button>
       </div>
     </div>
